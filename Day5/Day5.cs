c263082a0683baf3066bbe1fbe8561718966dae1
Foitn <marijnfeijten@live.nl>
2022-12-26 12:26:49 +0100

    Initial setup of day 5

---
+++ Day5/Day5.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2022.Day5;
+
+public sealed class Day5 : Day
+{
+    public Day5()
+    {
+        this.ReadPuzzleInput("Day5\\PuzzleInputDay5.txt");
+    }
+
+    public override void SolvePartA()
+    {
+    }
+
+    public override void SolvePartB()
+    {
+    }
+}
